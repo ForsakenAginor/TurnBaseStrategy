8704ef3bfe2711ac78a21c8d7820200e19176351
ForsakenAginor <juriys2022@mail.ru>
2024-12-01 02:32:46 +0400

    add animation controller

---
+++ Assets/Source/Scripts/Units/WalkableUnits/UnitAnimationController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class UnitAnimationController : MonoBehaviour
+{
+    private const string IsAttacking = nameof(IsAttacking);
+    private const string IsDying = nameof(IsDying);
+    private const string IsWalking = nameof(IsWalking);
+
+    [SerializeField] private Animator _animator;
+
+    public void Walk() => _animator.SetBool(IsWalking, true);
+
+    public void Stop() => _animator.SetBool(IsWalking, false);
+}
