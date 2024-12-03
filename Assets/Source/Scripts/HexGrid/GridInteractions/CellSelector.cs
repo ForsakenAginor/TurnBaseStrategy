d0cd0044a2c02cb8b8d25a1a1cfde3261b55054f
ForsakenAginor <juriys2022@mail.ru>
2024-12-03 04:02:12 +0400

    add touch support to grid navigation

--- Assets/Source/Scripts/HexGrid/GridInteractions/CellSelector.cs
+++ Assets/Source/Scripts/HexGrid/GridInteractions/CellSelector.cs
@@ -1,21 +1,34 @@
 ﻿using Assets.Scripts.HexGrid;
+using Lean.Touch;
 using System;
 using UnityEngine;
 
 public class CellSelector : MonoBehaviour
 {
+    [SerializeField] private LeanFingerDown _fingerDown;
+
     private HexGridXZ<CellSprite> _hexGrid;
     private GridRaycaster _gridRaycaster;
     private bool _isWorking = false;
 
     public event Action<Vector3, Vector2Int> CellClicked;
 
-    private void FixedUpdate()
+    private void Start()
+    {
+        _fingerDown.OnScreen.AddListener(OnFingerDown);
+    }
+
+    private void OnDestroy()
+    {
+        _fingerDown.OnScreen.RemoveListener(OnFingerDown);
+    }
+
+    private void OnFingerDown(Vector2 screenPosition)
     {
         if (_isWorking == false)
             return;
 
-        if(Input.GetMouseButtonDown(0) && _gridRaycaster.TryGetPointerPosition(out Vector3 worldPosition))
+        if (_gridRaycaster.TryGetPointerPosition(screenPosition, out Vector3 worldPosition))
         {
             var position = _hexGrid.GetXZ(worldPosition);
 
