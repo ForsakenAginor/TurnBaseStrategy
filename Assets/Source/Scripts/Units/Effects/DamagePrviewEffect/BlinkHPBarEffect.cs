1fb3563a074aa166b32e1988e9b19395e9636a7f
ForsakenAginor <juriys2022@mail.ru>
2025-01-25 02:33:26 +0400

    blink hp bar effect

---
+++ Assets/Source/Scripts/Units/Effects/DamagePrviewEffect/BlinkHPBarEffect.cs
@@ -0,0 +1,26 @@
+using DG.Tweening;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlinkHPBarEffect : MonoBehaviour
+{
+    [SerializeField] private Slider _bottomSlider;
+    [SerializeField] private Slider _topSlider;
+    [SerializeField] private Image _image;
+    [SerializeField] private Color _first;
+    [SerializeField] private Color _next;
+    [SerializeField] private float _frequence;
+
+    private Tween _tween;
+
+    [Button]
+    public void PlayEffect(float max, float current, float next)
+    {
+        _tween?.Kill();
+        _image.color = _first;
+        _bottomSlider.value = current / max;
+        _topSlider.value = next / max;
+        _tween = _image.DOColor(_next, _frequence).SetLoops(-1, LoopType.Yoyo);
+    }
+}
