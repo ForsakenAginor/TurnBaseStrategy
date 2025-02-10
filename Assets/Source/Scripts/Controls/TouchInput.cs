1a9a352e6cc4781c0ba8ea31465fc4419b0d7f9d
ForsakenAginor <juriys2022@mail.ru>
2025-02-10 18:47:25 +0400

    add WASD movement

--- Assets/Source/Scripts/Controls/TouchInput.cs
+++ Assets/Source/Scripts/Controls/TouchInput.cs
@@ -5,23 +5,27 @@ using UnityEngine;
 
 public class TouchInput : MonoBehaviour, ITouchInputReceiver
 {
+    private readonly KeyboardCameraMovement _keyboardCameraMovement = new KeyboardCameraMovement();
+
     [SerializeField] private LeanFingerFilter Use = new LeanFingerFilter(true);
+    [SerializeField] private float _keyboardSensitivity = 0.1f;
     [SerializeField] private float _sensitivityPC = 0.1f;
     [SerializeField] private float _sensitivityMobile = 0.2f;
     [SerializeField] private float _clampValue = 50f;
 
     private float _sensitivity;
     private bool _isSwiping;
+    private bool _isMobile;
 
     public event Action<Vector3> TouchInputReceived;
     public event Action TouchInputStopped;
 
     private void Start()
     {
-        bool isMobile = Application.isMobilePlatform &&
+        _isMobile = Application.isMobilePlatform &&
             (Application.platform == RuntimePlatform.WebGLPlayer || Application.platform == RuntimePlatform.Android);
 
-        if (isMobile == false)
+        if (_isMobile == false)
         {
             _sensitivity = _sensitivityPC;
             _clampValue = 3f;
@@ -34,6 +38,14 @@ public class TouchInput : MonoBehaviour, ITouchInputReceiver
 
     private void FixedUpdate()
     {
+        if (_isMobile == false)
+        {
+            Vector3 keyboardInput = _keyboardCameraMovement.GetInput();
+
+            if (keyboardInput != Vector3.zero)
+                TouchInputReceived?.Invoke(keyboardInput * _keyboardSensitivity);
+        }
+
         //anti pinch system
         if (LeanTouch.Fingers.Count != 2)
             return;
@@ -61,6 +73,17 @@ public class TouchInput : MonoBehaviour, ITouchInputReceiver
         Vector3 input = new Vector3(delta.x, 0, delta.y).normalized * _sensitivity;
         TouchInputReceived?.Invoke(input);
         _isSwiping = true;
+    }
+}
+
+public class KeyboardCameraMovement
+{
+    private const string Horizontal = nameof(Horizontal);
+    private const string Vertical = nameof(Vertical);
+
+    public Vector3 GetInput()
+    {
+        return new Vector3(-Input.GetAxis(Horizontal), 0, -Input.GetAxis(Vertical));
     }
 }
 
